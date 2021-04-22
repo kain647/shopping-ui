61f26e4d332160bb17ee747ed43f402dec6e3a6d
Aleksey <aleksey.amelchenko@gmail.com>
2021-04-22 18:01:35 +0300

    Initial commit from Create React App

---
+++ src/spinner/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import {
+  SpinnerContainer,
+  SpinnerButton,
+  SpinnerValue,
+} from "../spinner/styled";
+
+class Spinner extends React.Component {
+  state = {
+    value: this.props.initial || 1,
+  };
+
+  dec = () => {
+    const { onChange } = this.props;
+    const { value } = this.state;
+    const newValue = value - 1;
+    const finalValue = newValue < 0 ? 0 : newValue;
+    this.setState({
+      value: finalValue,
+    });
+    if (onChange) {
+      onChange(finalValue);
+    }
+  };
+
+  inc = () => {
+    const { value } = this.state;
+    const newValue = value + 1;
+    const finalValue = newValue > 10 ? 10 : newValue;
+    this.setState({
+      value: finalValue,
+    });
+    if (this.props.onChange) {
+      this.props.onChange(finalValue);
+    }
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <SpinnerContainer>
+        <SpinnerButton onClick={this.dec}>
+          <FiChevronLeft />
+        </SpinnerButton>
+        <SpinnerValue>{value}</SpinnerValue>
+        <SpinnerButton onClick={this.inc}>
+          <FiChevronRight />
+        </SpinnerButton>
+      </SpinnerContainer>
+    );
+  }
+}
+
+export default Spinner;
